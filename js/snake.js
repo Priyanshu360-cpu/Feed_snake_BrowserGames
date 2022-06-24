01b807bb579771519fe308cf3232fa030f81a3ee
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-24 12:16:00 +0530

    fixed coords

--- js/snake.js
+++ js/snake.js
@@ -8,10 +8,12 @@ function draw(){
         ctx.fillRect(680, 90, 40, 40);
         ctx.fillStyle="yellow";
         ctx.fillRect(920, 90, 20, 20);
+        let l=0;
         let previous = {
            "x": 680,
-           "y": 40,
+           "y": 90,
            "z":40,
+              "w":40
         }
         let food={
             "x": 920,
@@ -19,34 +21,49 @@ function draw(){
             "z":20,
         }
         setInterval(()=>{
-            ctx.clearRect(previous.x, previous.y,previous.z,40);
+            ctx.clearRect(previous.x, previous.y,previous.z,previous.w);
             if(previous.x>=(680+320)){
                 previous.x=320;
+                ctx.strokeRect(320, 50, 680, 680);
+            }
+            if(previous.y>=(680+50)){
+                previous.y=50;
                 ctx.strokeRect(320, 50, 680, 680);
             }
 if(previous.x==food.x && previous.y==food.y){
     let points=document.getElementById("points");
     points.innerHTML=parseInt(points.innerHTML)+1;
+    if(l=0){
     previous.z=previous.z+40;
+    }else{
+        previous.w=previous.w+40;
+    }
     ctx.clearRect(food.x, food.y,food.z,food.z);
     food.x=Math.floor(Math.random()*(1000-320))+320;
     food.y=Math.floor(Math.random()*(730-50))+50;
     ctx.fillStyle="yellow";
     ctx.fillRect(food.x, food.y,food.z,food.z);
 }
+if(l=0){
             previous.x=previous.x+10;
-           
+}else{
+    previous.y=previous.y+10;
+}
             console.log(previous.x)
             
             ctx.fillStyle="green";
-            ctx.fillRect(previous.x, previous.y, previous.z,40);
+            ctx.fillRect(previous.x, previous.y, previous.z,previous.w);
             
         },100)
         keyDown = ()=>{
             console.log("down")
         }
+        keyRight = ()=>{
+            console.log("right")
+        }
         window.addEventListener('keydown', keyDown);
-        previous.y=previous.y+50;
+        window.addEventListener('keyright', keyRight);
+     l=1;
         
     }
 }
